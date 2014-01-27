e4e9943a5c51f731bb31236234c24d7a3df14857
ilarina <ilarina@ilarina.(none)>
2014-01-27 20:33:59 -0200

    primeiro comite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Obra::Application.config.secret_key_base = '312a67b581d0814bec09fdd7ea74166061646ce6d9607f64de43999afffbad83a5c73dd8862a02d88cb092a6d6fdccb6e24b2dd36a6baae72ae762d31ab601c5'
